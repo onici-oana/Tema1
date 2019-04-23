e3b3f383100690bbef453ae530e572948995ad7c
onici-oana <onici.oana@yahoo.com>
2019-04-23 18:01:01 +0300

    3.

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
- int n=6, k=1; //am incercat metoda pe exemplul din tema
-      while(n!=1) {
-          if (n % 2 == 0) {
-              n = n / 2;
-              k++;
-          } else {
-              n = 3 * n + 1;
-              k++;
-          }
-      }
-        System.out.println("Numarul de pasi este:" +k);
+        //Exerciul 3
+        int n=5,max=0,k=0;
+        int[] array=new int[]{12,303,121,44,88};
+        for(int i=0;i<n;i++)
+        { if(array[i]%2!=0 && max<array[i])
+        {
+            max=array[i]; k=1;
+        }
+        }
 
+        if(k!=0)
+            System.out.println(max);
+        else
+            System.out.println("NO");
 
     }
 }
