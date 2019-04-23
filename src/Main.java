2b5b3756709170e688efaf102f863ee2477fcd07
onici-oana <onici.oana@yahoo.com>
2019-04-23 19:19:45 +0300

    13.

--- src/Main.java
+++ src/Main.java
@@ -2,76 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-      //Exercitiul 12 Var 2
+      //Exercitiul 13
 
-        int x=9;
-        if(x==0) {
-            System.out.println("Este luna ianuarie");}
-        else
+        int n=4;
+        for(int i=0;i<n;i++)
         {
-            if(x==1) {
-                System.out.println("Este luna februarie");
-            }
-            else{
-                if(x==2)
-                {System.out.println("Este luna martie");}
-                else
-                {
-                    if(x==3)
-                    {System.out.println("Este luna aprilie");}
-                    else
-                    {if(x==4)
-                    {System.out.println("Este luna mai");}
-                    else
-                    {
-                        if(x==5)
-                        {System.out.println("Este luna iunie");}
-                        else
-                        {if(x==6)
-                        {System.out.println("Este luna iulie");}
-                        else
-                        {if(x==7)
-                        {System.out.println("Este luna august");}
-                        else
-                        {
-                            if(x==8)
-                            {System.out.println("Este luna septembrie");}
-                            else
-                            {if(x==9)
-                            {System.out.println("Este luna octombrie");}
-                            else
-                            {
-                                if(x==10)
-                                {System.out.println("Este luna noiembrie");}
-                                else
-                                {
-                                    if(x==11)
-                                    {System.out.println("Este luna decembrie");}
-                                    else
-                                        System.out.println("Nu exista aceasta luna");
-
-
-
-
-                                }
-
-                            }
-
-
-                            }
-
-                        }
-
-                        }
-
-                        }
-                    }
-
-                    }
-                }
-            }
+            System.out.print("GO ");
         }
-
+        System.out.println("GATA");
     }
 }
 
