db21f6a1bfa058c0514bb6f7a4eb60fd44f278e5
onici-oana <onici.oana@yahoo.com>
2019-04-23 18:16:07 +0300

    5.

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Exerciul 4
-        int n=7,par=0,impar=0;
-        int[] array= new int[]{12,35,75,23,75,23,64};
-        for(int i=0;i<n;i++)
-        { if(array[i]%2==0)
-            par++;
-        else
-            impar++;
+        //Exercitiul 5
 
+        int n=24;
+        for(int i=n;i>=1;i--)
+        {
+            System.out.print(i+" ");
         }
-        System.out.println("In vector exista: " + par + " numere pare si " +impar + " numere impare");
-
     }
 }
 
