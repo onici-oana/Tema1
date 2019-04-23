c26e97f413e8d5a0188656202747e05d6ad2a895
onici-oana <onici.oana@yahoo.com>
2019-04-23 18:32:15 +0300

    7.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Exercitiul 6
-
-        int n=6,x=1;
-        for(int i=1;i<=n;i++)
-        {
-            System.out.print(x+ " ");
-            x=x+2;
+       //Exercitiul 7
+        int n=3,s=1;
+        while(n!=0)
+        { s=s*3;
+          n--;
         }
+
+        System.out.println(2*s);
+
     }
 }
 
