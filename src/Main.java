85f39405a0f2ac42634fa909d95a401163b7a220
onici-oana <onici.oana@yahoo.com>
2019-04-23 17:27:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x=1;
+        switch(x)
+        {
+            case 1: System.out.print("# "); x++;
+            case 2: System.out.print("## ");x++;
+            case 3: System.out.print("### "); x++;
+            case 4 :System.out.print("#### "); x++;
+            case 5: System.out.print("### "); x++;
+            case 6: System.out.print("## "); x++;
+            case 7: System.out.print("# "); x++;
+                break;
+        }
+            }
+        }
+
+
