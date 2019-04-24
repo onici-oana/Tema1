b2665ab5a2b2f9d135ed9d8e84bcf5eb4c192ce1
onici-oana <onici.oana@yahoo.com>
2019-04-24 14:14:16 +0300

    16.

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Exercitiul 15
+        //Exercitiul 16
 
-        int n=12,m=21,pal=0;
-
-        while(n!=0)
+        int n=24632,k=0,s=0;
+        int aux=n;
+        while(aux!=0)
         {
-            pal=pal*10+n%10;
-            n=n/10;
-        }
-        if(pal==m)
-            System.out.println("true");
+            aux=aux/10;
+            k++;
 
+        }
+
+        System.out.println("Numarul are "+ k +" cifre");
+        for(int i=k-1;i>=0;i++) {
+            if (i % 2 == 0)
+            {
+               s= s+n%10; n=n/10;
+            }
             else
-            System.out.println("false");
-
-
-
+                n=n/10;
+        }
+        System.out.println("Suma numerelor de pe pozitii pare este "+s);
 
 
         }
-
-
-
 }
 
 
