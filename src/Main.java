5f8c819557562b82a6ee4cdce6d4301a227b1f3e
onici-oana <onici.oana@yahoo.com>
2019-04-24 14:24:59 +0300

    18.

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Exercitiul 17
+        //Exercitiul 18
 
-        int n=1234,m=0,k=0,sp=0,si=0;
-        int aux=n;
-        while(aux!=0)
-        {
-            aux=aux/10;
-            k++;
-        }
+        char n = 'S';
+        if(n=='N' || n=='S' || n=='V' || n=='E'||n=='n' || n=='s' || n=='v' || n=='e')
+            System.out.println(n+ " este un punct cardinal");
+        else
+            System.out.println("n nu este o litera ce corespunde punctelor cardinale");
 
-        for(int i=k-1;i>=0;i++) {
-            if (i % 2 == 0)
-            {
-                sp= sp+n%10; n=n/10;
-            }
-            else
-            {  si=si+n%10; n=n/10;}
-        }
 
-        System.out.println(m=sp-si);
 
 
     }
